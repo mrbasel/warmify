a02b305703c615002444f17aae6d0131c86c0b5d
Basel <baselalnassr8@gmail.com>
2024-04-27 13:40:37 +0300

    implement notification functionality

--- warmify/warmify/settings.py
+++ warmify/warmify/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "warmify_core.middlewares.NotificationsMiddleware",
 ]
 
 ROOT_URLCONF = "warmify.urls"
