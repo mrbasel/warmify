5ac51cacc6571f0fa2d0a51b131c889222a8180f
Basel <baselalnassr8@gmail.com>
2024-06-02 12:45:42 +0300

    refactor populate events script

--- warmify/populate_events.py
+++ warmify/populate_events.py
@@ -1,6 +1,13 @@
 import os
+import argparse
 from datetime import datetime
 from random import randint
+
+parser = argparse.ArgumentParser(description="generate 50 random events")
+parser.add_argument("id", type=int, help="device id")
+
+args = parser.parse_args()
+device_id = args.id
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "warmify.settings")
 import django
@@ -14,5 +21,5 @@ for i in range(50):
         today.year, today.month, today.day, randint(6, 20), randint(0, 59)
     )
     Event.objects.create(
-        device_id=1, timestamp=timestamp, usage_milliliters=randint(10, 1000)
+        device_id=device_id, timestamp=timestamp, usage_milliliters=randint(10, 1000)
     )
